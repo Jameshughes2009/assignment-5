9f7c78129af54c7bf5c4b84a4f7707f54f2009b1
James Hughes <jameshughes2009@hotmail.com>
2024-02-03 12:01:03 -0500

    working on time updats

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,11 @@
-console.log(dayjs()) // Adding to Testlink with HTML
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var localeSettings = {};
+const localeSettings = {};
 dayjs.locale(localeSettings);
 $(function () {
-  var currentHour = dayjs().format("H")
+  const currentHour = dayjs().format("H")
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -13,13 +13,55 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   function timeColor(){
-    $(".time-block").each(function(){
-      var greyHour = parseInt(this.id);
+    $('.time-block').each(function(){
+      const blockHour = parseInt(this.id);
       $(this).toggleClass("past", blockHour < currentHour);
       $(this).toggleClass("present", blockHour === currentHour)
       $(this).toggleClass("future", blockHour > currentHour)
+    });
+  }
+  
+  function textEntry(){
+    $(".saveBtn").on('click', function(){
+      const key = $(this).parent().attr("id")
+      const value = $(this).siblings(".description").val();
+      localStorage.setItem(key, value);
     })
   }
+
+  function refreshColor() {
+    $('.time-block').each(function() {
+      const blockHour = parseInt(this.id);
+      if (blockHour == currentHour) {
+        $(this).removeClass('past future').addClass('present');
+      } else if (blockHour < currentHour) {
+        $(this).removeClass('future present').addClass('past');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
+  // Adding Functionailty to save cant figure out the timeing for color changes
+
+  $('.time-block').each(function() {
+    const key = $(this).attr('id');
+    const value = localStorage.getItem(key);
+    $(this).children('.description').val(value);
+  });
+
+    function refreshColor() {
+    $('.time-block').each(function() {
+      const blockHour = parseInt(this.id);
+      if (blockHour == currentHour) {
+        $(this).removeClass('past future').addClass('present');
+      } else if (blockHour < currentHour) {
+        $(this).removeClass('future present').addClass('past');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -30,6 +72,19 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  function updateTime() {
+    var dateElement = $('#date');
+    var timeElement = $('#time');
+    var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    var currentTime = dayjs().format('hh:mm:ss A');
+    dateElement.text(currentDate);
+    timeElement.text(currentTime);
+  }
   //
   // TODO: Add code to display the current date in the header of the page.
+  timeColor()
+  textEntry()
+  refreshColor();
+
+  setInterval(updateTime, 1000)
 });
