9cbc640432a2c90930ca147f24e07f4fc58ec5de
James Hughes <jameshughes2009@hotmail.com>
2024-02-03 09:35:23 -0500

    more module 5 work

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,4 @@
+console.log(dayjs()) // Adding to Testlink with HTML
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
